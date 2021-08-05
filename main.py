f28dc60293cd25d4d6ce6710c7508401d2a238d4
Jacob Odero <jackjax617@gmail.com>
2021-08-05 16:54:51 +0300

    refactor code to remove class

--- main.py
+++ main.py
@@ -5,129 +5,112 @@ from datetime import datetime
 import time
 import random
 
+
+uname = input('Enter Instagram Username: ')
+pwd = input('Enter Instagram Password: ')
+search_term = input('Enter Hashtag: ')
+
+print('\n Processing.......')
+
 driver = webdriver.Firefox()
-
-
-class Bot:
-
-    def __init__(self, user, pwd):
-        self.username = user
-        self.password = pwd
-
-    def log_in(self, user_inp, pwd_inp):
-        user_inp.send_keys(self.username)
-        pwd_inp.send_keys(self.password)
-        pwd_inp.send_keys(Keys.ENTER)
-
-
-    def like_pics(self, like_elm, next_elm, close_elm, num_of_pics):
-        while True:
-
-            driver.implicitly_wait(30)
-
-            for i in range(num_of_pics):
-                driver.implicitly_wait(10)
-                like_elm.click()
-                print('Liked ❤')
-
-                driver.implicitly_wait(10)
-                next_elm.click()
-
-            close_elm.click()
-            time.sleep(random.randint(3, 7))
-            driver.refresh()
-
-            # if datetime.now == 10:
-            #     break
-
-
-# ask for username and password
-username = input('Enter Username: ')
-password = input('Enter password: ')
-
-term = input('Enter hashtag to search: ')
-print('\n')
-
 driver.get('https://www.instagram.com/')
 
-# instanciate Bot class
-bot = Bot(username, password)
 
-driver.implicitly_wait(5)
+# Element selectors
+username_selector = 'div.-MzZI:nth-child(1) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)'
+password_selector = 'div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)'
 
-# username input
-uname_path = 'div.-MzZI:nth-child(1) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)'
-uname = driver.find_element(By.CSS_SELECTOR, uname_path)
+# not now button 1 and 2
+not_now_selector = 'button.sqdOP:nth-child(4)'
+not_now2_selector = 'button.aOOlW:nth-child(2)'
 
-# password input
-pwd_path = 'div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)'
-pwd = driver.find_element(By.CSS_SELECTOR, pwd_path)
+# search input
+search_selector = '.eyXLr'
+search_selector2 = '.XTCLo'
+
+# results from search
+results_xpath = '/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div'
+
+# navigate to recent posts div
+recent_div_xpath = '/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]'
+recent_div_xpath2 = '/html/body/div[1]/section/main/article/div[1]/div/div/div[3]'
+
+# like button
+like_selector = '.fr66n > button:nth-child(1)'
+like_xpath = '/html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button'
+like_xpath2 = '/html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button'
+
+# next button
+next_selector = '._65Bje'
+next_xpath = '/html/body/div[6]/div[1]/div/div/a[2]'
+
+# close button
+close_selector = '.BI4qX > button:nth-child(1)'
+close_xpath = '/html/body/div[6]/div[3]/button'
+
+
+
+##################################################
+# functionality
+##################################################
+
 
 # log in to instagram
-bot.log_in(uname, pwd)
+driver.implicitly_wait(10)
+driver.find_element(By.CSS_SELECTOR, username_selector).send_keys(uname)
+driver.find_element(By.CSS_SELECTOR, password_selector).send_keys(pwd)
 
-# close modals
-driver.implicitly_wait(30)
+driver.find_element(By.CSS_SELECTOR, password_selector).send_keys(Keys.ENTER)
 
-not_now = driver.find_element(By.CSS_SELECTOR, 'button.sqdOP:nth-child(4)')
-driver.implicitly_wait(5)
-not_now.click()
 
-driver.implicitly_wait(30)
-
-driver.find_element(By.CSS_SELECTOR, 'button.aOOlW:nth-child(2)').click()
+# close instagram modal popups
+driver.implicitly_wait(10)
+driver.find_element(By.CSS_SELECTOR, not_now_selector).click()
+driver.implicitly_wait(10)
+driver.find_element(By.CSS_SELECTOR, not_now2_selector).click()
 
 
 # search for hashtag
-search = driver.find_element(By.CSS_SELECTOR, '.eyXLr')
 driver.implicitly_wait(10)
-search.click()
-
+search = driver.find_element(By.CSS_SELECTOR, search_selector).click()
 driver.implicitly_wait(10)
-search = driver.find_element(By.CSS_SELECTOR, '.XTCLo')
+driver.find_element(By.CSS_SELECTOR, search_selector2).send_keys(search_term)
+driver.find_element(By.CSS_SELECTOR, search_selector2).send_keys(Keys.ENTER)
 
-search.send_keys(term)
-search.send_keys(Keys.ENTER)
-driver.find_elements_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div')[0].click()
 
-driver.implicitly_wait(10)
-driver.find_elements_by_xpath('/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]')[0].click()
-# /html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]
+# select search result
+driver.find_elements_by_xpath(results_xpath)[0].click()
 
-like_elm = driver.find_element(By.CSS_SELECTOR, '.fr66n > button:nth-child(1)')
-# .fr66n > button:nth-child(1)
-# /html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button
+# click on recent posts
+driver.implicitly_wait(20)
+driver.find_elements_by_xpath(recent_div_xpath)[0].click()
 
-next_elm = driver.find_element(By.CSS_SELECTOR, '._65Bje')
-# ._65Bje
 
-close_elm = driver.find_element(By.CSS_SELECTOR, '.BI4qX > button:nth-child(1)')
-# .BI4qX > button:nth-child(1)
-
-driver.implicitly_wait(30)
-# bot.like_pics(like_elm, next_elm, close_elm, 4)
+# like pictures
+like_btn = driver.find_element(By.CSS_SELECTOR, like_selector)
+next_btn = driver.find_element(By.CSS_SELECTOR, next_selector)
+close_btn = driver.find_element(By.CSS_SELECTOR, close_selector)
 
 while True:
     for i in range(4):
         driver.implicitly_wait(10)
-        like_elm.click()
+        like_btn.click()
         print('Liked ❤')
-        
+
         driver.implicitly_wait(10)
-        next_elm.click()
-        
-        like_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+        next_btn.click()
+
+        like_btn = driver.find_element(By.XPATH, like_xpath)
 
     driver.implicitly_wait(10)
-    close_elm.click()
+    close_btn.click()
 
     driver.refresh()
-    time.sleep(random.randint(4, 10))
-    driver.find_elements_by_xpath('/html/body/div[1]/section/main/article/div[1]/div/div/div[3]')[0].click()
-    like_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button')
-    next_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div/a[2]')
-    close_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/button')
-        
+    time.sleep(random.randint(3, 5))
 
+    driver.find_elements_by_xpath(recent_div_xpath2)[0].click()
 
+    like_btn = driver.find_element(By.XPATH, like_xpath2)
+    next_btn = driver.find_element(By.XPATH, next_xpath)
+    close_btn = driver.find_element(By.XPATH, close_xpath)
 
