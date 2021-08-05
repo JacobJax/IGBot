58894a68c2a8962ea95fb579e0ac18e9599d4b67
Jacob Odero <jackjax617@gmail.com>
2021-08-05 10:30:35 +0300

    to be edited

--- main.py
+++ main.py
@@ -22,21 +22,23 @@ class Bot:
 
     def like_pics(self, like_elm, next_elm, close_elm, num_of_pics):
         while True:
-            driver.implicitly_wait(30)
-            for i in range(num_of_pics):
-                driver.implicitly_wait(30)
-                like_elm.click()
 
-                driver.implicitly_wait(30)
+            driver.implicitly_wait(30)
+
+            for i in range(num_of_pics):
+                driver.implicitly_wait(10)
+                like_elm.click()
+                print('Liked ❤')
+
+                driver.implicitly_wait(10)
                 next_elm.click()
 
-                close_elm.click()
-                # driver.refresh()
-                # driver.implicitly_wait(30)
-
+            close_elm.click()
             time.sleep(random.randint(3, 7))
-            if datetime.now == 10:
-                break
+            driver.refresh()
+
+            # if datetime.now == 10:
+            #     break
 
 
 # ask for username and password
@@ -44,6 +46,7 @@ username = input('Enter Username: ')
 password = input('Enter password: ')
 
 term = input('Enter hashtag to search: ')
+print('\n')
 
 driver.get('https://www.instagram.com/')
 
@@ -88,20 +91,43 @@ search.send_keys(Keys.ENTER)
 driver.find_elements_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div')[0].click()
 
 driver.implicitly_wait(10)
-driver.find_elements_by_xpath('/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]')[0].click()
+driver.find_elements_by_xpath('/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]')[0].click()
+# /html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]
 
-driver.implicitly_wait(30)
 like_elm = driver.find_element(By.CSS_SELECTOR, '.fr66n > button:nth-child(1)')
+# .fr66n > button:nth-child(1)
+# /html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button
 
-driver.implicitly_wait(30)
 next_elm = driver.find_element(By.CSS_SELECTOR, '._65Bje')
+# ._65Bje
 
-driver.implicitly_wait(30)
 close_elm = driver.find_element(By.CSS_SELECTOR, '.BI4qX > button:nth-child(1)')
+# .BI4qX > button:nth-child(1)
 
 driver.implicitly_wait(30)
-bot.like_pics(like_elm, next_elm, close_elm, 4)
+# bot.like_pics(like_elm, next_elm, close_elm, 4)
 
+while True:
+    for i in range(4):
+        driver.implicitly_wait(10)
+        like_elm.click()
+        print('Liked ❤')
+        
+        driver.implicitly_wait(10)
+        next_elm.click()
+        
+        like_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+
+    driver.implicitly_wait(10)
+    close_elm.click()
+
+    driver.refresh()
+    time.sleep(random.randint(4, 10))
+    driver.find_elements_by_xpath('/html/body/div[1]/section/main/article/div[1]/div/div/div[3]')[0].click()
+    like_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+    next_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div/a[2]')
+    close_elm = driver.find_element(By.XPATH, '/html/body/div[6]/div[3]/button')
+        
 
 
 
